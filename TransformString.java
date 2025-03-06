b1c9b8e26c169171ef80b24590566bd869d3556a
Rohit <contact.rohit99@gmail.com>
2025-03-06 21:46:27 +0530

    Transform String Persistent Sys

---
+++ TransformString.java
@@ -0,0 +1,22 @@
+//Replace first occurrence of letter o with single $, second occurence with 2 $ and so on.
+public class TransformString {
+
+    public static void main(String[] args) {
+        String str = "Tomorrowo";
+
+        int count =0;
+        int print;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) =='o'){
+                count++;
+                print = count;
+                while(print>0){
+                    System.out.print("$");
+                    print--;
+                }
+            }else{
+                System.out.print(str.charAt(i));
+            }
+        }
+    }
+}
